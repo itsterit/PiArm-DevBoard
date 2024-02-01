5b5024033d50c798a89a77a5847e8b01779abf05
itsterit <itsterit@gmail.com>
2024-02-01 22:18:02 +0400

    Горит светодиод, надо разобраться с тактированием, ибо хуй пойми что там по шинам тактирования

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -3,11 +3,24 @@
 #include <stm32f103xb.h>
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
-  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
+#warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
 int main(void)
 {
-    /* Loop forever */
-	for(;;);
+
+  /**
+   * @brief Отладочный светодиод
+   *        22 пин - PB11
+   */
+  RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
+  GPIOB->CRH &= ~(GPIO_CRH_CNF11_Msk); 
+  GPIOB->CRH |= (0b11 << GPIO_CRH_MODE11_Pos); 
+  GPIOB->ODR |= (0b01 << GPIO_ODR_ODR11_Pos);
+
+  /* Loop forever */
+  while (true)
+  {
+    asm("NOP");
+  }
 }
