7ddd8183df6cb0d425cbb51c7816cdf4588ddc6a
itsterit <itsterit@gmail.com>
2024-02-01 18:50:07 +0400

    Добавил CMSIS в пустой проект

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <main.h>
+#include <stm32f103xb.h>
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
