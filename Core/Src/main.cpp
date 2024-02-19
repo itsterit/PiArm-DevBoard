c6395aeca9d539f413f04c383b517cd6fe7f8c7c
itsterit <itsterit@gmail.com>
2024-02-19 22:27:30 +0400

    Хрень с внешним кварцем

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -25,22 +25,21 @@ int main(void)
   clock_control::hsi::enable(true);
   clock_control::hse::enable(true);
   clock_control::pll::enable(false);
-  if (clock_control::hse::ready())
+  if (clock_control::hsi::ready)
   {
-    clock_control::pll::hse_clock_divided(false);
-    clock_control::pll::pll_clock_source(clock_control::PLL_CLOCK_SOURCE_Type::HSE_oscillator);
-
-    clock_control::set_ahb_prescaler(2);
-    clock_control::pll::multiplication_factor(clock_control::MULTIPLICATION_FACTOR_Type::PLL_INPUT_CLOCK_X4);
+    clock_control::pll::pll_clock_source(clock_control::PLL_CLOCK_SOURCE_Type::HSI_oscillator_divided_by_2);
+    clock_control::pll::multiplication_factor(clock_control::MULTIPLICATION_FACTOR_Type::PLL_INPUT_CLOCK_X9);
     clock_control::pll::enable(true);
     if (clock_control::pll::ready())
+    {
       if (clock_control::clock_switch(clock_control::PLL_SELECTED_AS_SYSTEM_CLOCK))
-        led_pin.set_config(GPIO::input_pull_up);
+      {
+        led_pin.set();
+      }
+    }
   }
   else
   {
-    led_pin.set_config(GPIO::output_push_pull);
-    led_pin.set();
     NVIC_SystemReset();
   }
 
