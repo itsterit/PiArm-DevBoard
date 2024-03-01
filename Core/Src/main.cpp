b2297f63187c8e9feb892bbde27d640ff8d25c96
itsterit <itsterit@gmail.com>
2024-03-01 17:29:56 +0400

    конфиг таймер классом

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -53,18 +53,21 @@ int main(void)
     }
   }
 
+  RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
+  AFIO->MAPR &= ~AFIO_MAPR_TIM3_REMAP_Msk;
+  AFIO->MAPR |= (0b01 << AFIO_MAPR_TIM3_REMAP_PARTIALREMAP_Pos);
   coil_frequency_timer.set_channel_output_config(2U, OUTPUT_COMPARE_CLEAR_DISABLE, OUTPUT_COMPARE_PRELOAD_ENABLE, OUTPUT_COMPARE_FAST_ENABLE, CHANNEL_PWM_MODE_1);
   coil_frequency_timer.set_event_generation(TRIGGER_GENERATION_DISABLE, UPDATE_GENERATION_DISABLE, 0);
   coil_frequency_timer.set_dma_interrupt_config(TRIGGER_DMA_REQUEST_DISABLE, UPDATE_DMA_REQUEST_DISABLE, TRIGGER_INTERRUPT_DISABLE, UPDATE_INTERRUPT_ENABLE, 0, (TIM_DIER_CC3IE_Msk | TIM_DIER_CC2IE_Msk));
   coil_frequency_timer.slave_mode_control(INTERNAL_TRIGGER0, SLAVE_MODE_DISABLED);
   coil_frequency_timer.master_mode_config(MASTER_MODE_COMPARE_PULSE);
   coil_frequency_timer.capture_compare_register(0, TIM_CCER_CC2E_Msk);
+  coil_frequency_timer.set_timer_config(400, 300, 100, 0, 1000, 71, 0);
   coil_frequency_timer.set_counter_config(ARR_REGISTER_BUFFERED, COUNTER_UPCOUNTER, ONE_PULSE_DISABLE, COUNTER_ENABLE);
+  NVIC_EnableIRQ(TIM3_IRQn);
 
   set_tmr1_cfg();
   NVIC_EnableIRQ(TIM1_UP_IRQn);
-  set_tmr3_cfg();
-  NVIC_EnableIRQ(TIM3_IRQn);
 
   while (true)
   {
@@ -176,24 +179,4 @@ void set_tmr1_cfg(void)
   TIM1->CR1 |= (0b00 << TIM_CR1_URS_Pos);  // Update request source
   TIM1->CR1 |= (0b00 << TIM_CR1_UDIS_Pos); // Update disable
   // TIM1->CR1 |= (0b01 << TIM_CR1_CEN_Pos);  // Counter enable
-}
-
-void set_tmr3_cfg(void)
-{
-
-  /**
-   *  set PB5 as TIM3_CH2
-   *  use apb1(72MHz) as timer clock
-   */
-  RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
-  AFIO->MAPR &= ~AFIO_MAPR_TIM3_REMAP_Msk;
-  AFIO->MAPR |= (0b01 << AFIO_MAPR_TIM3_REMAP_PARTIALREMAP_Pos);
-
-  TIM3->CCR1 = 400; // TIMx capture/compare register - таймер 1 + смещение анализ сигнала
-  TIM3->CCR2 = 300; // TIMx capture/compare register - шим
-  TIM3->CCR3 = 100; // TIMx capture/compare register - таймер 1 + смещение на замер тока
-
-  TIM3->ARR = 1000; // auto-reload register
-
-  TIM3->PSC = 71; // Prescaler value
 }
