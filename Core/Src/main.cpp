8cc939dea6d3d76730e8755874008db63e4937f3
itsterit <itsterit@gmail.com>
2024-03-01 17:39:47 +0400

    конфиг таймер классом 2

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -14,9 +14,6 @@ GPIO gen_freq(GPIOB, 5U);
 timer coil_frequency_timer(TIM3);
 timer sampling_timer(TIM1);
 
-void set_tmr3_cfg(void);
-void set_tmr1_cfg(void);
-
 int main(void)
 {
   led_pin.clock_enable(true);
@@ -53,6 +50,12 @@ int main(void)
     }
   }
 
+  sampling_timer.set_dma_interrupt_config(TRIGGER_DMA_REQUEST_DISABLE, UPDATE_DMA_REQUEST_DISABLE, TRIGGER_INTERRUPT_DISABLE, UPDATE_INTERRUPT_ENABLE, 0, 0);
+  sampling_timer.slave_mode_control(INTERNAL_TRIGGER2, TRIGGER_MODE);
+  sampling_timer.set_timer_config(0, 0, 0, 0, 5, 72, 0);
+  sampling_timer.set_counter_config(ARR_REGISTER_BUFFERED, COUNTER_UPCOUNTER, ONE_PULSE_DISABLE, COUNTER_DISABLE);
+  NVIC_EnableIRQ(TIM1_UP_IRQn);
+
   RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
   AFIO->MAPR &= ~AFIO_MAPR_TIM3_REMAP_Msk;
   AFIO->MAPR |= (0b01 << AFIO_MAPR_TIM3_REMAP_PARTIALREMAP_Pos);
@@ -65,9 +68,6 @@ int main(void)
   coil_frequency_timer.set_timer_config(400, 300, 100, 0, 1000, 71, 0);
   coil_frequency_timer.set_counter_config(ARR_REGISTER_BUFFERED, COUNTER_UPCOUNTER, ONE_PULSE_DISABLE, COUNTER_ENABLE);
   NVIC_EnableIRQ(TIM3_IRQn);
-
-  set_tmr1_cfg();
-  NVIC_EnableIRQ(TIM1_UP_IRQn);
 
   while (true)
   {
@@ -104,79 +104,4 @@ extern "C" void TIM3_IRQHandler(void)
   }
 
   TIM3->SR = ~TIM3->SR;
-}
-
-void set_tmr1_cfg(void)
-{
-  TIM1->CR2 = 0x00;
-  TIM1->CR2 |= (0b00 << TIM_CR2_OIS4_Pos);  // Output Idle state x (OCx output)
-  TIM1->CR2 |= (0b00 << TIM_CR2_OIS3N_Pos); // Output Idle state x (OCxN output)
-  TIM1->CR2 |= (0b00 << TIM_CR2_OIS3_Pos);  // Output Idle state x (OCx output)
-  TIM1->CR2 |= (0b00 << TIM_CR2_OIS2N_Pos); // Output Idle state x (OCxN output)
-  TIM1->CR2 |= (0b00 << TIM_CR2_OIS2_Pos);  // Output Idle state x (OCx output)
-  TIM1->CR2 |= (0b00 << TIM_CR2_OIS1N_Pos); // Output Idle state x (OCxN output)
-  TIM1->CR2 |= (0b00 << TIM_CR2_OIS1_Pos);  // Output Idle state x (OCx output)
-  TIM1->CR2 |= (0b00 << TIM_CR2_TI1S_Pos);  // TI1 selection
-  TIM1->CR2 |= (0b00 << TIM_CR2_MMS_Pos);   // !Master mode selection
-  TIM1->CR2 |= (0b00 << TIM_CR2_CCDS_Pos);  // Capture/compare DMA selection
-  TIM1->CR2 |= (0b00 << TIM_CR2_CCUS_Pos);  // Capture/compare control update selection
-  TIM1->CR2 |= (0b00 << TIM_CR2_CCPC_Pos);  // Capture/compare preloaded control
-
-  TIM1->SMCR = 0x00;
-  TIM1->SMCR |= (0b00 << TIM_SMCR_ETP_Pos);  // External trigger polarity
-  TIM1->SMCR |= (0b00 << TIM_SMCR_ECE_Pos);  // External clock enable
-  TIM1->SMCR |= (0b00 << TIM_SMCR_ETPS_Pos); // External trigger prescaler
-  TIM1->SMCR |= (0b00 << TIM_SMCR_ETF_Pos);  // External trigger filter
-  TIM1->SMCR |= (0b00 << TIM_SMCR_MSM_Pos);  // !Master/slave mode
-  TIM1->SMCR |= (0b010 << TIM_SMCR_TS_Pos);  // Trigger selection( Internal Trigger 2 (ITR2) = TIM3_TRGO)
-  TIM1->SMCR |= (0b110 << TIM_SMCR_SMS_Pos); // !Slave mode selection
-
-  TIM1->DIER = 0x00;
-  TIM1->DIER |= (0b00 << TIM_DIER_TDE_Pos);   // Trigger DMA request enable
-  TIM1->DIER |= (0b00 << TIM_DIER_COMDE_Pos); // COM DMA request enable
-  TIM1->DIER |= (0b00 << TIM_DIER_CC4DE_Pos); // Capture/Compare DMA request enable
-  TIM1->DIER |= (0b00 << TIM_DIER_CC3DE_Pos); // Capture/Compare DMA request enable
-  TIM1->DIER |= (0b00 << TIM_DIER_CC2DE_Pos); // Capture/Compare DMA request enable
-  TIM1->DIER |= (0b00 << TIM_DIER_CC1DE_Pos); // Capture/Compare DMA request enable
-  TIM1->DIER |= (0b00 << TIM_DIER_UDE_Pos);   // Update DMA request enable
-  TIM1->DIER |= (0b00 << TIM_DIER_BIE_Pos);   // Break interrupt enable
-  TIM1->DIER |= (0b00 << TIM_DIER_TIE_Pos);   // Trigger interrupt enable
-  TIM1->DIER |= (0b00 << TIM_DIER_COMIE_Pos); // COM interrupt enable
-  TIM1->DIER |= (0b00 << TIM_DIER_CC4IE_Pos); // Capture/Compare interrupt enable
-  TIM1->DIER |= (0b00 << TIM_DIER_CC3IE_Pos); // Capture/Compare interrupt enable
-  TIM1->DIER |= (0b00 << TIM_DIER_CC2IE_Pos); // Capture/Compare interrupt enable
-  TIM1->DIER |= (0b00 << TIM_DIER_CC1IE_Pos); // Capture/Compare interrupt enable
-  TIM1->DIER |= (0b01 << TIM_DIER_UIE_Pos);   // Update interrupt enable
-
-  TIM1->EGR = 0x00;
-  TIM1->EGR |= (0b00 << TIM_EGR_BG_Pos);   // Break generation
-  TIM1->EGR |= (0b00 << TIM_EGR_TG_Pos);   // Trigger generation
-  TIM1->EGR |= (0b00 << TIM_EGR_COMG_Pos); // Capture/Compare control update generation
-  TIM1->EGR |= (0b00 << TIM_EGR_CC4G_Pos); // Capture/Compare generation
-  TIM1->EGR |= (0b00 << TIM_EGR_CC3G_Pos); // Capture/Compare generation
-  TIM1->EGR |= (0b00 << TIM_EGR_CC2G_Pos); // Capture/Compare generation
-  TIM1->EGR |= (0b00 << TIM_EGR_CC1G_Pos); // Capture/Compare generation
-  TIM1->EGR |= (0b00 << TIM_EGR_UG_Pos);   // Update generation
-
-  TIM1->CCMR1 = 0x00;
-
-  TIM1->CCMR2 = 0x00;
-
-  TIM1->CCER = 0x00;
-
-  TIM1->CNT = 0x00;
-
-  TIM1->PSC = 72;
-
-  TIM1->ARR = 5;
-
-  TIM1->CR1 = 0x00;
-  TIM1->CR1 |= (0b00 << TIM_CR1_CKD_Pos);  // Clock division
-  TIM1->CR1 |= (0b01 << TIM_CR1_ARPE_Pos); // Auto-reload preload enable
-  TIM1->CR1 |= (0b00 << TIM_CR1_CMS_Pos);  // Center-aligned mode selection
-  TIM1->CR1 |= (0b00 << TIM_CR1_DIR_Pos);  // Direction
-  TIM1->CR1 |= (0b00 << TIM_CR1_OPM_Pos);  // One pulse mode
-  TIM1->CR1 |= (0b00 << TIM_CR1_URS_Pos);  // Update request source
-  TIM1->CR1 |= (0b00 << TIM_CR1_UDIS_Pos); // Update disable
-  // TIM1->CR1 |= (0b01 << TIM_CR1_CEN_Pos);  // Counter enable
 }
