c1bf0f07988a32ad7a928c1c77234898366922f5
itsterit <itsterit@gmail.com>
2024-03-16 09:42:19 +0400

    буду добавлять ацп

--- Core/Src/handlers/modbus_handler.cpp
+++ Core/Src/handlers/modbus_handler.cpp
@@ -1,5 +1,9 @@
 #include <main.h>
 
+/**
+ * @brief   Отправка ответа мастер устройству по modbus
+ * @note    По идее нт защит от перезаписи :D
+*/
 void ModBusTxCallback(uint8_t *DataPtr, int16_t DataSize)
 {
     usb_as_dma_transmit((uint8_t *)&DataPtr[0], DataSize);
