2984b0143e82f1524774f227a93af04033c96f57
itsterit <itsterit@gmail.com>
2024-03-30 15:45:15 +0400

    марафет ацп кр2

--- Core/Lib/adc/adc_cr1.h
+++ Core/Lib/adc/adc_cr1.h
@@ -3,76 +3,76 @@
 
 typedef enum
 {
-    REGULAR_CHANNELS_ANALOG_WATCHDOG_DISABLED                    = 0b00,
-    REGULAR_CHANNELS_ANALOG_WATCHDOG_ENABLED                     = 0b01,
+    AWDEN__REGULAR_CHANNELS_ANALOG_WATCHDOG_DISABLED                      = 0b00,
+    AWDEN__REGULAR_CHANNELS_ANALOG_WATCHDOG_ENABLED                       = 0b01,
 } ADC_CR1_AWDEN_Type;
 
 typedef enum
 {
-    INJECTED_CHANNELS_ANALOG_WATCHDOG_DISABLED                   = 0b00,
-    INJECTED_CHANNELS_ANALOG_WATCHDOG_ENABLED                    = 0b01,
+    JAWDEN__INJECTED_CHANNELS_ANALOG_WATCHDOG_DISABLED                    = 0b00,
+    JAWDEN__INJECTED_CHANNELS_ANALOG_WATCHDOG_ENABLED                     = 0b01,
 } ADC_CR1_JAWDEN_Type;
 
 typedef enum
 {
-    INDEPENDENT_MODE                                             = 0b0000,
-    COMBINED_REGULAR_SIMULTANEOUS_AND_INJECTED_SIMULTANEOUS_MODE = 0b0001,
-    COMBINED_REGULAR_SIMULTANEOUS_AND_ALTERNATE_TRIGGER_MODE     = 0b0010,
-    COMBINED_INJECTED_SIMULTANEOUS_AND_FAST_INTERLEAVED_MODE     = 0b0011,
-    COMBINED_INJECTED_SIMULTANEOUS_AND_SLOW_INTERLEAVED_MODE     = 0b0100,
-    INJECTED_SIMULTANEOUS_MODE_ONLY                              = 0b0101,
-    REGULAR_SIMULTANEOUS_MODE_ONLY                               = 0b0110,
-    FAST_INTERLEAVED_MODE_ONLY                                   = 0b0111,
-    SLOW_INTERLEAVED_MODE_ONLY                                   = 0b1000,
-    ALTERNATE_TRIGGER_MODE_ONLY                                  = 0b1001,
+    DUALMOD__INDEPENDENT_MODE                                             = 0b0000,
+    DUALMOD__COMBINED_REGULAR_SIMULTANEOUS_AND_INJECTED_SIMULTANEOUS_MODE = 0b0001,
+    DUALMOD__COMBINED_REGULAR_SIMULTANEOUS_AND_ALTERNATE_TRIGGER_MODE     = 0b0010,
+    DUALMOD__COMBINED_INJECTED_SIMULTANEOUS_AND_FAST_INTERLEAVED_MODE     = 0b0011,
+    DUALMOD__COMBINED_INJECTED_SIMULTANEOUS_AND_SLOW_INTERLEAVED_MODE     = 0b0100,
+    DUALMOD__INJECTED_SIMULTANEOUS_MODE_ONLY                              = 0b0101,
+    DUALMOD__REGULAR_SIMULTANEOUS_MODE_ONLY                               = 0b0110,
+    DUALMOD__FAST_INTERLEAVED_MODE_ONLY                                   = 0b0111,
+    DUALMOD__SLOW_INTERLEAVED_MODE_ONLY                                   = 0b1000,
+    DUALMOD__ALTERNATE_TRIGGER_MODE_ONLY                                  = 0b1001,
 } ADC_CR1_DUALMOD_Type;
 
 typedef enum
 {
-    INJECTED_CHANNELS_DISCONTINUOUS_MODE_DISABLED                = 0b00,
-    INJECTED_CHANNELS_DISCONTINUOUS_MODE_ENABLED                 = 0b01,
+    JDISCEN__INJECTED_CHANNELS_DISCONTINUOUS_MODE_DISABLED                = 0b00,
+    JDISCEN__INJECTED_CHANNELS_DISCONTINUOUS_MODE_ENABLED                 = 0b01,
 } ADC_CR1_JDISCEN_Type;
 
 typedef enum
 {
-    REGULAR_CHANNELS_DISCONTINUOUS_MODE_DISABLED                 = 0b00,
-    REGULAR_CHANNELS_DISCONTINUOUS_MODE_ENABLED                  = 0b01,
+    DISCEN__REGULAR_CHANNELS_DISCONTINUOUS_MODE_DISABLED                  = 0b00,
+    DISCEN__REGULAR_CHANNELS_DISCONTINUOUS_MODE_ENABLED                   = 0b01,
 } ADC_CR1_DISCEN_Type;
 
 typedef enum
 {
-    AUTOMATIC_INJECTED_CONVERSION_DISABLED                       = 0b00,
-    AUTOMATIC_INJECTED_CONVERSION_ENABLED                        = 0b01,
+    JAUTO__AUTOMATIC_INJECTED_CONVERSION_DISABLED                         = 0b00,
+    JAUTO__AUTOMATIC_INJECTED_CONVERSION_ENABLED                          = 0b01,
 } ADC_CR1_JAUTO_Type;
 
 typedef enum
 {
-    ANALOG_WATCHDOG_ON_ALL_CHANNELS                              = 0b00,
-    ANALOG_WATCHDOG_ON_SINGLE_CHANNEL                            = 0b01,
+    AWDSGL__ANALOG_WATCHDOG_ON_ALL_CHANNELS                               = 0b00,
+    AWDSGL__ANALOG_WATCHDOG_ON_SINGLE_CHANNEL                             = 0b01,
 } ADC_CR1_AWDSGL_Type;
 
 typedef enum
 {
-    SCAN_MODE_DISABLED                                           = 0b00,
-    SCAN_MODE_ENABLED                                            = 0b01,
+    SCAN__SCAN_MODE_DISABLED                                              = 0b00,
+    SCAN__SCAN_MODE_ENABLED                                               = 0b01,
 } ADC_CR1_SCAN_Type;
 
 typedef enum
 {
-    JEOC_INTERRUPT_DISABLED                                      = 0b00,
-    JEOC_INTERRUPT_ENABLED                                       = 0b01,
+    JEOCIE__JEOC_INTERRUPT_DISABLED                                       = 0b00,
+    JEOCIE__JEOC_INTERRUPT_ENABLED                                        = 0b01,
 } ADC_CR1_JEOCIE_Type;
 
 typedef enum
 {
-    ANALOG_WATCHDOG_INTERRUPT_DISABLED                           = 0b00,
-    ANALOG_WATCHDOG_INTERRUPT_ENABLED                            = 0b01,
+    AWDIE__ANALOG_WATCHDOG_INTERRUPT_DISABLED                             = 0b00,
+    AWDIE__ANALOG_WATCHDOG_INTERRUPT_ENABLED                              = 0b01,
 } ADC_CR1_AWDIE_Type;
 
 typedef enum
 {
-    EOC_INTERRUPT_DISABLED                                       = 0b00,
-    EOC_INTERRUPT_ENABLED                                        = 0b01,
+    EOCIE__EOC_INTERRUPT_DISABLED                                         = 0b00,
+    EOCIE__EOC_INTERRUPT_ENABLED                                          = 0b01,
 } ADC_CR1_EOCIE_Type;
 
 #endif /* __ADC_CR1_H__ */
--- Core/Lib/adc/adc_cr2.h
+++ Core/Lib/adc/adc_cr2.h
@@ -1,59 +1,125 @@
 #ifndef __ADC_CR2_H__
 #define __ADC_CR2_H__
 
+/**
+ * Включить каналы : 
+ * встроенный датчик температуры,
+ * встроенный источник опорного напряжения
+ */
 typedef enum
 {
-
+    TSVREFE__TEMPERATURE_SENSOR_VREFINT_CHANNEL_DISABLED = 0b00,
+    TSVREFE__TEMPERATURE_SENSOR_VREFINT_CHANNEL_ENABLED  = 0b01,
 } ADC_CR2_TSVREFE_Type;
 
+/**
+ * Если стоит программный запуск -
+ * запустить регулярные преобразования
+ */
 typedef enum
 {
-
+    SWSTART__REGULAR_CHANNELS_RESET                      = 0b0,
+    SWSTART__REGULAR_CHANNELS_STARTS                     = 0b1,
 } ADC_CR2_SWSTART_Type;
 
+/**
+ * Если стоит программный запуск -
+ * запустить встраиваемые преобразования
+ */
 typedef enum
 {
-
+    JSWSTART__INJECTED_CHANNELS_RESET                    = 0b0,
+    JSWSTART__INJECTED_CHANNELS_STARTS                   = 0b1,
 } ADC_CR2_JSWSTART_Type;
 
+/**
+ * Установка и выбор источника запуска
+ * регулярного преобразования
+ */
 typedef enum
 {
-
+    EXTTRIG__CONVERSION_ON_EXTERNAL_EVENT_DISABLED       = 0b0,
+    EXTTRIG__CONVERSION_ON_EXTERNAL_EVENT_ENABLED        = 0b1,
 } ADC_CR2_EXTTRIG_Type;
 
 typedef enum
 {
+    EXTSEL__TIMER_1_CC1_EVENT                            = 0b000,
+    EXTSEL__TIMER_1_CC2_EVENT                            = 0b001,
+    EXTSEL__TIMER_1_CC3_EVENT                            = 0b010,
+    EXTSEL__TIMER_2_CC2_EVENT                            = 0b011,
+    EXTSEL__TIMER_3_TRGO_EVENT                           = 0b100,
+    EXTSEL__TIMER_4_CC4_EVENT                            = 0b101,
+    EXTSEL__EXTI_LINE_11_OR_TIM8_TRGO_EVENT              = 0b110,
+    EXTSEL__SWSTART                                      = 0b111,
+} ADC_CR2_EXTSEL_Type;
 
+/**
+ * Установка и выбор источника запуска
+ * встраиваемого преобразования
+ */
+typedef enum
+{
+    JEXTTRIG__CONVERSION_ON_EXTERNAL_EVENT_DISABLED      = 0b00,
+    JEXTTRIG__CONVERSION_ON_EXTERNAL_EVENT_ENABLED       = 0b01,
 } ADC_CR2_JEXTTRIG_Type;
 
 typedef enum
 {
+    JEXTSEL__TIMER_1_TRGO_EVENT                          = 0b000,
+    JEXTSEL__TIMER_1_CC4_EVENT                           = 0b001,
+    JEXTSEL__TIMER_2_TRGO_EVENT                          = 0b010,
+    JEXTSEL__TIMER_2_CC1_EVENT                           = 0b011,
+    JEXTSEL__TIMER_3_CC4_EVENT                           = 0b100,
+    JEXTSEL__TIMER_4_TRGO_EVENT                          = 0b101,
+    JEXTSEL__EXTI_LINE15_OR_TIM8_CC4_EVENT               = 0b110,
+    JEXTSEL__JSWSTART                                    = 0b111,
+} ADC_CR2_JEXTSEL_Type;
 
+/**
+ * Выравнивание результатов преобразования
+ */
+typedef enum
+{
+    ALIGN__RIGHT_ALIGNMENT                               = 0b0,
+    ALIGN__LEFT_ALIGNMENT                                = 0b1,
 } ADC_CR2_ALIGN_Type;
 
+/**
+ * Использование модуля дма
+ * Только для АЦП1 и АЦП3
+ */
 typedef enum
 {
-
+    DMA__DMA_MODE_DISABLED                               = 0b0,
+    DMA__DMA_MODE_ENABLED                                = 0b1,
 } ADC_CR2_DMA_Type;
 
+/**
+ * Сброс калибровки
+ */
 typedef enum
 {
-
+    RSTCAL__CALIBRATION_REGISTER_INITIALIZED             = 0b0,
+    RSTCAL__INITIALIZE_CALIBRATION_REGISTER              = 0b1,
 } ADC_CR2_RSTCAL_Type;
 
+/**
+ * Выбор режима запуска
+ */
 typedef enum
 {
-
-} ADC_CR2_CAL_Type;
-
-typedef enum
-{
-
+    CONT__SINGLE_CONVERSION_MODE                         = 0b0,
+    CONT__CONTINUOUS_CONVERSION_MODE                     = 0b1,
 } ADC_CR2_CONT_Type;
 
+/**
+ * запуск АЦП
+ */
 typedef enum
 {
-
+    ADON__DISABLE_ADC                                    = 0b0,
+    ADON__ENABLE_ADC                                     = 0b1,
 } ADC_CR2_ADON_Type;
 
 #endif /* __ADC_CR2_H__ */
