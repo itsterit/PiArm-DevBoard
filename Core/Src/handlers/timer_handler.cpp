1a7791ed24b0727e7a89d237f45c25bc4f59e3fe
itsterit <itsterit@gmail.com>
2024-05-10 11:26:32 +0400

    Питонишка - робит + стабильно. буду увеличивать ток с катушки на МК

--- Core/Src/handlers/timer_handler.cpp
+++ Core/Src/handlers/timer_handler.cpp
@@ -36,7 +36,7 @@ void set_timer_config()
         coil_frequency_timer.slave_mode_control(INTERNAL_TRIGGER0, SLAVE_MODE_DISABLED);
         coil_frequency_timer.master_mode_config(MASTER_MODE_COMPARE_PULSE);
         coil_frequency_timer.capture_compare_register(0, TIM_CCER_CC2E_Msk);
-        set_generation_timing(1000000, 500, 1);
+        set_generation_timing(1000000, 500, 5);
         coil_frequency_timer.set_counter_config(ARR_REGISTER_BUFFERED, COUNTER_UPCOUNTER, ONE_PULSE_DISABLE, COUNTER_ENABLE);
     }
 }
@@ -80,12 +80,13 @@ extern "C" void TIM3_IRQHandler(void)
         if (TIM3->SR & TIM_SR_CC4IF_Msk)
         {
             /* Конец замера ответа катушки */
-            uint16_t rem_smp = DMA1_Channel1->CNDTR;
-            usInputRegisters[9] = (100 - rem_smp);
-            adc_samling_dma.dma_start(100, (uint32_t *)&usInputRegisters[10], (uint32_t *)&ADC1->DR);
+            
+            // uint16_t rem_smp = DMA1_Channel1->CNDTR;
+            // usInputRegisters[9] = (100 - rem_smp);
+            // adc_samling_dma.dma_start(100, (uint32_t *)&usInputRegisters[10], (uint32_t *)&ADC1->DR);
+            // TIM3->CR1 &= ~(TIM_CR1_CEN_Msk);
 
             TIM1->CR1 &= ~(TIM_CR1_CEN_Msk);
-            TIM3->CR1 &= ~(TIM_CR1_CEN_Msk);
             TIM1->SR = ~TIM1->SR;
             TIM1->CNT = 0;
         }
